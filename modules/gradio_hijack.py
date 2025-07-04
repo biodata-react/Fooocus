d6cb544b4e0826383a08461251a8b94eb28a4c1f
biodata-react <122708718+biodata-react@users.noreply.github.com>
2025-07-04 11:31:47 +0300

    Update gradio_hijack.py Blocks

--- modules/gradio_hijack.py
+++ modules/gradio_hijack.py
@@ -465,13 +465,13 @@ class Image(
 
 all_components = []
 
-if not hasattr(Block, 'original__init__'):
-    Block.original_init = Block.__init__
+if not hasattr(Blocks, 'original__init__'):
+    Blocks.original_init = Blocks.__init__
 
 
 def blk_ini(self, *args, **kwargs):
     all_components.append(self)
-    return Block.original_init(self, *args, **kwargs)
+    return Blocks.original_init(self, *args, **kwargs)
 
 
 Block.__init__ = blk_ini
